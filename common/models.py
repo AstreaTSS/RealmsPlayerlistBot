6ad02d109adf491bf79d659d87aa25aba3c9dff9
Astrea49 <25420078+Astrea49@users.noreply.github.com>
2022-09-16 20:41:38 -0400

    feat: get rid of remaining legacy prefix code

--- common/models.py
+++ common/models.py
@@ -6,20 +6,6 @@ from tortoise.contrib.postgres.fields import ArrayField
 from tortoise.models import Model
 
 
-class SetField(ArrayField, set):
-    """A somewhat exploity way of using an array field to store a set."""
-
-    def to_python_value(self, value):
-        value = None if value is None else set(value)
-        self.validate(value)
-        return value
-
-    def to_db_value(self, value, _):
-        self.validate(value)
-        value = None if value is None else list(value)
-        return value
-
-
 class GuildConfig(Model):
     class Meta:
         table = "realmguildconfig"
