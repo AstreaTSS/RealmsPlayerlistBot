006699fac7bd890e2d755384ac70244ae5aed4c6
Astrea49 <25420078+Astrea49@users.noreply.github.com>
2023-01-01 21:40:05 -0500

    fix: copy dict before iterating

--- exts/owner_cmds.py
+++ exts/owner_cmds.py
@@ -307,7 +307,7 @@ class OwnerCMDs(utils.Extension):
 
     @naff.prefixed_command(aliases=["reloadallextensions"])
     async def reload_all_extensions(self, ctx: naff.PrefixedContext):
-        for ext in (e.extension_name for e in self.bot.ext.values()):
+        for ext in (e.extension_name for e in self.bot.ext.copy().values()):
             self.bot.reload_extension(ext)
         await ctx.reply("All extensions reloaded!")
 
