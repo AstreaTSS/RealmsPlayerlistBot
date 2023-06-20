ee6d6cfae7af07c7c8c420c1cb32399529bd0dda
Astrea49 <25420078+Astrea49@users.noreply.github.com>
2023-06-20 16:25:57 -0400

    style: oops

--- common/xbox_api/__init__.py
+++ common/xbox_api/__init__.py
@@ -1,4 +1,4 @@
 from .club_models import *  # noqa: F403
-from .core import *  # noqa: F403\
+from .core import *  # noqa: F403
 from .peoplehub_models import *  # noqa: F403
 from .profile_models import *  # noqa: F403
