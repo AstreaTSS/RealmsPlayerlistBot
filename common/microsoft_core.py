e9627159e31ede032ab7c1564e5ff0006abcad42
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2023-06-28 11:36:09 -0400

    fix: retry as specified by microsoft

--- common/microsoft_core.py
+++ common/microsoft_core.py
@@ -1,5 +1,6 @@
 import asyncio
 import datetime
+import functools
 import typing
 
 import aiohttp
@@ -290,15 +291,6 @@ class AuthenticationManager:
         await self.session.close()
 
 
-@add_decoder
-class ThrottledResponse(ParsableCamelModel):
-    version: int
-    current_requests: int
-    max_requests: int
-    period_in_seconds: int
-    limit_type: str
-
-
 class MicrosoftAPIException(Exception):
     def __init__(self, resp: aiohttp.ClientResponse, error: Exception) -> None:
         self.resp = resp
@@ -321,7 +313,7 @@ class CustomRetry(aiohttp_retry.JitterRetry):
         if not response:
             return super().get_timeout(attempt, response)
 
-        if response.status == 401:
+        if response.headers.get("Retry-After"):
             timeout = self._start_timeout
             self._start_timeout = 0.5
             result = super().get_timeout(attempt, response)
@@ -331,17 +323,14 @@ class CustomRetry(aiohttp_retry.JitterRetry):
         return super().get_timeout(attempt, response)
 
 
-async def evaluate_response_callback(resp: aiohttp.ClientResponse) -> bool:
-    if resp.status == 401:  # unauthorized
-        if retry_after := resp.headers.get("Retry-After"):
-            await asyncio.sleep(float(retry_after))
-        return False
-    if resp.status == 429:
-        try:
-            parsed_resp = await ThrottledResponse.from_response(resp)
-            await asyncio.sleep(parsed_resp.period_in_seconds)
-        except msgspec.ValidationError:
-            await asyncio.sleep(5)
+async def evaluate_response_callback(
+    auth_mgr: AuthenticationManager, resp: aiohttp.ClientResponse
+) -> bool:
+    if resp.status == 401:
+        await auth_mgr.refresh_tokens(force_refresh=True)
+
+    if retry_after := resp.headers.get("Retry-After"):
+        await asyncio.sleep(float(retry_after))
         return False
     return True
 
@@ -368,13 +357,15 @@ class BaseMicrosoftAPI:
                 attempts=3,
                 start_timeout=0.1,
                 random_interval_size=0.2,
-                evaluate_response_callback=evaluate_response_callback,
             ),
             response_class=BetterResponse,
             json_serialize=_orjson_dumps_wrapper,
         )
         auth_mgr = await AuthenticationManager.from_file(
             session, client_id, client_secret, cls.RELYING_PATH, oauth_path
+        )
+        session._retry_options.evaluate_response_callback = functools.partial(
+            evaluate_response_callback, auth_mgr
         )
         return cls(session, auth_mgr)
 
