4f588a0ea36b4aee8f9888f4e74edc8d0c4f7d65
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2023-08-22 17:30:34 -0400

    feat: shorten cooldown

--- exts/statistics.py
+++ exts/statistics.py
@@ -558,7 +558,7 @@ class Statistics(utils.Extension):
                 self.bot,
                 leaderboard_builder,
                 page_size=1000,
-                timeout=120,
+                timeout=60,
                 default_title=f"Leaderboard for the past {period_str}",
                 default_color=ctx.bot.color,
             )
