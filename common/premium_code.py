eba84a32d89fd5345bfa0de897b50e618005f157
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2024-01-19 23:37:26 -0500

    docs: add proper mit license for coupon_codes

--- common/premium_code.py
+++ common/premium_code.py
@@ -14,6 +14,31 @@ You should have received a copy of the GNU Affero General Public License along w
 Playerlist Bot. If not, see <https://www.gnu.org/licenses/>.
 """
 
+# code modified from https://github.com/brett-patterson/coupon_codes - 2015 Brett Patterson, MIT License
+"""
+The MIT License (MIT)
+
+Copyright (c) 2015 Brett Patterson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
 import codecs
 import re
 import secrets
@@ -23,7 +48,6 @@ import interactions as ipy
 
 __all__ = ("full_code_generate", "full_code_validate", "bytestring_length_decode")
 
-# code modified from https://github.com/brett-patterson/coupon_codes - 2015 Brett Patterson, MIT License
 
 BAD_WORDS: typing.Final[frozenset[str]] = frozenset(
     codecs.decode(w, "rot13")
