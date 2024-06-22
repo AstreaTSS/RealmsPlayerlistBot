a7c5d97a1ba4837aa0011a6cb845f14eafdf52c3
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2024-06-22 01:45:58 -0400

    feat: back to normal

--- exts/autorunners.py
+++ exts/autorunners.py
@@ -46,7 +46,6 @@ UPSELLS = [
         "Having issues with the bot? Check out the FAQ or join the support server:"
         " /support"
     ),
-    "Check out the new Mid June 2024 Update post: https://sh.astrea.cc/midjun24",
 ]
 LENGTH_UPSELLS = len(UPSELLS)
 
