e89d4eadcc4f3d049991b13fda6b6b711c8dae61
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2024-07-15 21:32:14 -0400

    feat: update note

--- exts/statistics.py
+++ exts/statistics.py
@@ -834,8 +834,8 @@ class Statistics(utils.Extension):
         playtime = await self.playtime_handle(ctx, period, gamertag)
 
         end_string = (
-            "per session\n-# Basically, they play, on average, for that much time every"
-            " time they join the Realm."
+            "per session\n-# ℹ️ Essentially, they play, on average, for that much time"  # noqa: RUF001
+            " every time they join the Realm."
             if gamertag
             else "per player"
         )
